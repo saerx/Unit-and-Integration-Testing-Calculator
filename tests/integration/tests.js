fdc281ab7d7a79b5bbcebf06ae44d522459bb8f9
saerx <saerlaith.robyn@gmail.com>
2021-01-24 11:54:33 +0000

    added integration test for large numbers

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -43,4 +43,27 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('36')
   })
 
+  it('should be able to function for large numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click()
+    element(by.css('#operator_multiply')).click()
+    element(by.css('#number4')).click()
+    element(by.css('#number0')).click()
+    element(by.css('#number0')).click()
+    element(by.css('#number0')).click()
+    element(by.css('#number0')).click()
+    element(by.css('#number0')).click()
+    element(by.css('#number0')).click()
+    element(by.css('#number0')).click()
+    element(by.css('#number0')).click()
+    element(by.css('#number0')).click()
+    element(by.css('#number0')).click()
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number1')).click()
+    element(by.css('#number2')).click()
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('119999999988')
+  })
+
+
 });
